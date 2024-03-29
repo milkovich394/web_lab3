9e4d6a0f3197366d126b82041e0254cfbe6b8e5f
ushenkina <106740523+ushenkina@users.noreply.github.com>
2024-03-29 23:30:02 +0500

    Update footerData.js

--- src/js/mockData/footerData.js
+++ src/js/mockData/footerData.js
@@ -1,13 +1,13 @@
 export const headerOfFooter = {
     header: "Хотите Шагнуть в Будущее Раньше Других?",
-    button:{
+    button: {
         href: "#",
         text: "Запросить ранний доступ",
     },
 };
 
 export const infoGpt3 = {
-    img:{
+    img: {
         src: "./assets/img/GPT-3.svg",
         alt: "not-found",
     },
@@ -16,71 +16,30 @@ export const infoGpt3 = {
     Все права защищены`,
 };
 
-export const footerLinks = {
-        title: "Ссылки",
-        items: [
-            {
-            href: "#",
-            text: "Овероны",
-            },
-            {
-            href: "#",
-            text: "Соц. сети",
-            },
-            {
-            href: "#",
-            text: "Счетчики",
-            },
-            {
-            href: "#",
-            text: "Контакты",
-            },
-        ],
-    };
+export const footerLinks = [
+    { href: "#", text: "Овероны" },
+    { href: "#", text: "Соц. сети" },
+    { href: "#", text: "Счетчики" },
+    { href: "#", text: "Контакты" },
+];
 
-export const companyLinks = 
-    {
-        title: "Компания",
-        items: [
-            {
-            href: "#",
-            text: "Условия использования",
-            },
-            {
-            href: "#",
-            text: "Перс. данные",
-            },
-            {
-            href: "#",
-            text: "Конаткты",
-            },
-        ],
-    };
+export const companyLinks = [
+    { href: "#", text: "Условия использования" },
+    { href: "#", text: "Перс. данные" },
+    { href: "#", text: "Конаткты" },
+];
 
-export const contactLinks = 
-    {
-        title: "Контакты",
-        items: [
-            {
-            href: "#",
-            text: "ул. Профессора Поздеева, 13, к.Г, Пермь",
-            },
-            {
-            href: "#",
-            text: "+7 (342) 2-198-520",
-            },
-            {
-            href: "#",
-            text: "info@pstu.ru",
-            },
-        ],
-    };
+export const contactLinks = [
+    { href: "#", text: "ул. Профессора Поздеева, 13, к.Г, Пермь" },
+    { href: "#", text: "+7 (342) 2-198-520" },
+    { href: "#", text: "info@pstu.ru" },
+];
 
-export const theEnd ={
+export const theEnd = {
     text: '© 2023 GPT-3. Все права защищены.',
 };
 
-export const footerData ={
+export const footerData = {
     headerOfFooter,
     infoGpt3,
     footerLinks,
